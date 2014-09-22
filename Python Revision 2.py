de98de43e6abc1f41f32de203e1849b36880221a
Alexander Allan <45302@student.longroad.ac.uk>
2014-09-22 12:17:26 +0100

    Pushed. 22/09/2014

--- Python Revision 2.py
+++ Python Revision 2.py
@@ -1,10 +1,9 @@
 #Alexander Allan
-#15/09/2014
-#Python Revision 1
+#16/09/2014
+#Python Revision 2
 
 Number_1 = int(input("Please enter the first number: "))
 Number_2 = int(input("Please enter the second number: "))
 Number_3 = int(input("Please enter the third number: "))
-Number_4 = int(input("Please enter the fourth number: "))
-Answer = int((Number_1 + Number_2 + Number_3 + Number_4))
+Answer = int((Number_1 * Number_2 / Number_3))
 print("The answer is {0}.".format(Answer))
---
+++ Python Revision 3.py
@@ -0,0 +1,8 @@
+#Alexander Allan
+#16/09/2014
+#Python Revision 3
+
+Number_1 = int(input("Please enter the first number: "))
+Number_2 = int(input("Please enter the second number: "))
+Answer = int((Number_1 % Number_2))
+print("{0} / {1} = {2}.".format(Number_1,Number_2,Answer))
